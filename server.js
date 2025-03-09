42289f541e0eeb7bfb1ebd0aa9207edaeb34b4d4
Fiijoey <kofimensah3730@gmail.com>
2025-03-09 23:55:30 +0000

    refactor: migrate server.js to ES module syntax and update MongoDB connection

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
 const app = express();
 const port = 8080;
+const urldb =
+  "mongodb+srv://Fiifi:fiifi@cluster0.kdphu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
+
 mongoose
-  .connect(
-    "mongodb+srv://Fiifi:Fiifi@cluster0.kdphu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(urldb, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
@@ -46,7 +46,7 @@ const professionalSchema = new mongoose.Schema({
 const Professional = mongoose.model("Professional", professionalSchema);
 
 // REST endpoint to serve the professional data from MongoDB
-app.get("/professional", async (req, res) => {
+app.get("/professional", async (_req, res) => {
   try {
     const data = await Professional.findOne(); // Retrieve the first document in the collection
     if (!data) {
